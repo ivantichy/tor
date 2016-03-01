ab056b2e126c8736c14bebb7bf6669dd9c38879a
ivantichy <ivan.tichy@gmail.com>
2016-03-02 00:54:02 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ sed -i 's/^#SocksPort.*9100.*/SocksPort 0.0.0.0:6001/' /etc/tor/torrc
 sed -i 's/^#Log notice file/Log notice file/' /etc/tor/torrc
 sed -i 's/^#RunAsDaemon.*/RunAsDaemon 0/' /etc/tor/torrc
 sed -i 's/^#ORPort 9001/ORPort 6000/' /etc/tor/torrc
-sed -i 's/^#Nickname.*/Nickname WWW.IVANTICHY.CZ/' /etc/tor/torrc
+sed -i 's/^#Nickname.*/Nickname IVANTICHY/' /etc/tor/torrc
 sed -i 's/^#RelayBandwidthRate/RelayBandwidthRate/' /etc/tor/torrc
 sed -i 's/^#RelayBandwidthBurst/RelayBandwidthBurst/' /etc/tor/torrc
 sed -i 's/^#ExitPolicy reject/ExitPolicy reject/' /etc/tor/torrc
