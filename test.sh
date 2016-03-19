85114d78d8e8d3ce913e53716cd5c288f2472b8e
Ivan Tichy <ivan.tichy@gmail.com>
2016-03-19 14:25:42 +0000

    some updates

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ sed -i 's/^#Log notice file/Log notice file/' /etc/tor/torrc
 sed -i 's/^#RunAsDaemon.*/RunAsDaemon 0/' /etc/tor/torrc
 sed -i 's/^#ORPort 9001/ORPort 6000/' /etc/tor/torrc
 sed -i 's/^#Nickname.*/Nickname WWW.IVANTICHY.CZ/' /etc/tor/torrc
-sed -i 's/^#RelayBandwidthRate/RelayBandwidthRate/' /etc/tor/torrc
-sed -i 's/^#RelayBandwidthBurst/RelayBandwidthBurst/' /etc/tor/torrc
+#sed -i 's/^#RelayBandwidthRate/RelayBandwidthRate/' /etc/tor/torrc
+#sed -i 's/^#RelayBandwidthBurst/RelayBandwidthBurst/' /etc/tor/torrc
 sed -i 's/^#ExitPolicy reject/ExitPolicy reject/' /etc/tor/torrc
 
 echo test
